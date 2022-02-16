cf071ccf4734e445ad3af180be8457c0253e841f
RCCT <rbrbcl3@gmail.com>
2022-02-15 22:10:00 -0500

    primer commit ws

---
+++ CalculadoraVBCliente/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtn1 = New System.Windows.Forms.TextBox()
+        Me.txtn2 = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.cbxOperacion = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtn1
+        '
+        Me.txtn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtn1.Location = New System.Drawing.Point(151, 20)
+        Me.txtn1.Name = "txtn1"
+        Me.txtn1.Size = New System.Drawing.Size(143, 26)
+        Me.txtn1.TabIndex = 0
+        '
+        'txtn2
+        '
+        Me.txtn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtn2.Location = New System.Drawing.Point(151, 71)
+        Me.txtn2.Name = "txtn2"
+        Me.txtn2.Size = New System.Drawing.Size(143, 26)
+        Me.txtn2.TabIndex = 1
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalcular.Location = New System.Drawing.Point(29, 158)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(265, 30)
+        Me.btnCalcular.TabIndex = 2
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'cbxOperacion
+        '
+        Me.cbxOperacion.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbxOperacion.FormattingEnabled = True
+        Me.cbxOperacion.Items.AddRange(New Object() {"Suma", "Resta", "Multiplicacion", "Division", "Coseno", "Seno", "Tangente", "Inverso", "Potencia", "RaizCuadrada", "Factorial"})
+        Me.cbxOperacion.Location = New System.Drawing.Point(29, 124)
+        Me.cbxOperacion.Name = "cbxOperacion"
+        Me.cbxOperacion.Size = New System.Drawing.Size(265, 28)
+        Me.cbxOperacion.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(25, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Numero 1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(25, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Numero 2:"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResultado.Location = New System.Drawing.Point(29, 194)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(265, 26)
+        Me.txtResultado.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(321, 245)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cbxOperacion)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtn2)
+        Me.Controls.Add(Me.txtn1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtn1 As TextBox
+    Friend WithEvents txtn2 As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents cbxOperacion As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtResultado As TextBox
+End Class
---
+++ CalculadoraVBCliente/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Dim service As New ServiceReference1.CalculadoraServiceSoapClient
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim nro1 = txtn1.Text, nro2 = txtn2.Text
+        Dim op = cbxOperacion.Text
+
+        Select Case op
+            Case "Suma"
+                txtResultado.Text = service.suma(nro1, nro2)
+            Case "Resta"
+                txtResultado.Text = service.Resta(nro1, nro2)
+            Case "Multiplicacion"
+                txtResultado.Text = service.Multiplicacion(nro1, nro2)
+            Case "Division"
+                txtResultado.Text = service.div(nro1, nro2)
+            Case "Coseno"
+                txtResultado.Text = service.Coseno(nro1)
+            Case "Seno"
+                txtResultado.Text = service.Seno(nro1)
+            Case "Tangente"
+                txtResultado.Text = service.Tangente(nro1)
+            Case "Inverso"
+                txtResultado.Text = service.Inverso(nro1)
+            Case "Potencia"
+                txtResultado.Text = service.Potencia(nro1, nro2)
+            Case "RaizCuadrada"
+                txtResultado.Text = service.RaizCuadrada(nro1, nro2)
+            Case "Factorial"
+                txtResultado.Text = service.fac(nro1)
+        End Select
+    End Sub
+End Class
